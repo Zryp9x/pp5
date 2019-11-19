81b0306213dd123c04ceb5eef529fef6df24efe6
Zryp9x <sneefeeh@gmail.com>
2019-11-19 17:21:14 +0100

    m

---
+++ src/main/java/pl/krakow/uek/pp5/creditcard/model/CreditCardFacade.java
@@ -0,0 +1,9 @@
+package pl.krakow.uek.pp5.creditcard.model;
+
+import pl.krakow.uek.pp5.creditcard.model.commands.WithdrawCommand;
+
+public class CreditCardFacade {
+    public void handle(WithdrawCommand withdrawCommand) {
+
+    }
+}
